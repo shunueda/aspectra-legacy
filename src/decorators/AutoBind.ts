fce697d4ec58ece93f7a7848ce7fa7e2a60f2256
Shun Ueda <me@shu.nu>
2024-09-18 00:22:03 -0400

    style: avoid arrow parens

--- src/decorators/AutoBind.ts
+++ src/decorators/AutoBind.ts
@@ -8,11 +8,9 @@ export function AutoBind<T extends AnyClass>(
     constructor(...args: AnyArgs) {
       super(...args)
       Object.getOwnPropertyNames(target.prototype)
-        .filter(
-          (key) => key !== 'constructor' && typeof this[key] === 'function',
-        )
-        .forEach((key) => {
-          this[key] = this[key].bind(this)
+        .filter(it => it !== 'constructor' && typeof this[it] === 'function')
+        .forEach(it => {
+          this[it] = this[it].bind(this)
         })
     }
   }
